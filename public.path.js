1e494564cf30f2dff755e84cc1c4dfdab2946a99
Kamoliddinbek <kamoliddinbekxudoyberdi@gmail.com>
2024-07-29 09:25:50 +0500

    create server function for path server

--- public.path.js
+++ public.path.js
@@ -1,12 +1,14 @@
 import * as http from "http";
+import { hostname } from "os";
 
 const usersData = new Map();
-const ports = new Set();
+// user: port
 
+const ports = new Set();
 
 function defineUser(req) {
   let user = req.url?.split("/")[1];
-  if(user) {
+  if (user) {
     return user;
   } else {
     return null;
@@ -18,12 +20,20 @@ function getRequestOptions(req) {
     headers: req.headers,
     method: req.method,
     path: url.path, //userning nomini url dan olib tashlash kerak
-    body: req?.body
-  }
+    body: req?.body,
+  };
 }
 
-function getSubRequest(path){
-
+function getSubRequest(user) {
+  return {
+    hostname: "localhost",
+    port: usersData.get(user),
+    path: "/request",
+    method: "POST",
+    headers: {
+      "Content-type": "application/json",
+    },
+  };
 }
 
 function getRandomPort(min = 1024, max = 65535) {
@@ -42,23 +52,65 @@ function getOpenPort() {
   return newPort;
 }
 
-function createServer(port){
+let eventRes = undefined;
+let resultRes = undefined;
+
+function createServer(port) {
   const subServer = http.createServer((req, res) => {
+    let url = req.url;
+    if(url === "/request") {
+      resultRes = res;
+      
+      let body = "";
+      resultRes.end("It is answer from sub server")
+      // req.on("data", (chunk) => {
+      //   body += chunk;
+      // });
+      
+      // req.on("end", () => {
+      //   if(!eventRes) {
+      //     res.status(404).end("Connection not found");
+      //   } else {
+      //     eventRes.write(body);
+      //   }
+      // })
+    }
     res.end("I am a sub server");
   });
 
   subServer.listen(port, () => {
     console.log(`Sub server is running on port ${port}`);
   });
+
+  return port;
 }
 
 const server = http.createServer((req, res) => {
-  if(req.url === "/create-server") {
-    createServer(2007);
+  if (req.url === "/create-server") {
+    let newServerPort = createServer(2007);
+    usersData.set("kamoliddin", newServerPort);
   }
-  res.end("hi")
+
+  let user = defineUser(req);
+
+  if(user && usersData.get(user)) {
+    let subReq = http.request(getSubRequest(user), (subRes) => {
+      let response = "";
+
+      subRes.on("data", (chunk) => {
+        response += chunk;
+      });
+
+      subRes.on("end", () => {
+        res.write(response);
+        res.end()
+      })
+    })
+  } else {
+    res.end("User not found");
+  }
 });
 
 server.listen(2006, () => {
   console.log("Server is running on port 2006");
-})
+});
